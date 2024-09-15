c88be3b4f37b89d5b96c88d1f2fa494dcbcd13aa
Melvin Chua <mchua@c2r5s1.42singapore.sg>
2024-09-15 18:17:00 +0800

    check the current_var

--- src/builtin_main.c
+++ src/builtin_main.c
@@ -6,7 +6,7 @@
 /*   By: mchua <mchua@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 12:40:05 by seayeo            #+#    #+#             */
-/*   Updated: 2024/09/15 16:21:40 by mchua            ###   ########.fr       */
+/*   Updated: 2024/09/15 18:11:01 by mchua            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -391,35 +391,41 @@ static int	args_key_counter(char *src)
 static	t_env	*get_env_loc(t_env *env_list, char *arg)
 {
 	char	*env_data;
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (arg[i] && arg[i] != '=')
+	{
+		count++;
+		i++;
+	}
 	while (env_list)
 	{
 		env_data = env_list->var;
-		while (*arg)
-		{
-			if (*env_data == *arg)
-			{
-				arg++;
-				env_data++;
-			}
-			else
-				break ;
-		}
+		if ((ft_strncmp(env_list->var, arg, count) == 0) && (ft_strncmp(env_list->var, arg, count + 1) == 0))
+				return (env_list);
 		env_list = env_list->next;
 	}
-	return (env_list);
+	return (NULL);
 }
 
-static t_var	*get_var_loc(char *arg, t_var *var_list)
+static t_var	*get_var_loc(char *arg, t_var *var_list, t_env *current_env)
 {
+	int	i;
 	int	count;
 	char	*var_data;
+	
+	i = 0;
+	count = 0;
 	if (!var_list)
 		return (NULL);
 	count = ft_strlen(arg);
 	while (var_list)
 	{
 		var_data = var_list->name;
-		if (ft_strncmp(var_data, arg, count) == 0 || ft_strchr(arg, '=') != NULL)
+		if (ft_strncmp(var_data, arg, count) == 0 && ft_strcmp(arg, current_env->var) != 0)
 			return (var_list);
 		var_list = var_list->next;
 	}
@@ -457,14 +463,14 @@ int	export_handler(t_shell *store)
 
 	current_env = store->env;
 	arg = store->cmd_head->command->next->data;
-	current_var = get_var_loc(arg, store->var);
 	if (is_in_env(current_env, arg, store))
 		{
 			current_env = get_env_loc(store->env, arg);
+			current_var = get_var_loc(arg, store->var, current_env);
 			free (current_env->var);
 			if (current_var)
 				current_env->var = ft_strdup(store->var->hidden);
-			else
+			else if (!current_var && ft_strchr(arg, '='))
 				current_env->var = ft_strdup(arg);
 		}
 	else
