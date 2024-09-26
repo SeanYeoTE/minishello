7f57642a5a07d57d441b47ed84a5235ff70156e2
Melvin Chua <mchua@c1r3s5.42singapore.sg>
2024-09-26 20:44:00 +0800

    fixed base unset

--- src/unset.c
+++ src/unset.c
@@ -22,13 +22,36 @@ static void	perform_unset(t_env *target_env, t_shell *store)
 	previous_node = store->env;
 	while (previous_node->next != target_env)
 		previous_node = previous_node->next;
-	if (previous_node->next->next != NULL)
-		next_node = previous_node->next->next;
+	if (target_env->next)
+		next_node = target_env->next;
 	else
 		next_node = NULL;
 	free (target_env->var);
 	free (target_env);
 	previous_node->next = next_node;
+}
+
+static t_env	*get_target_loc(t_env *env_list, char *arg)
+{
+	char	*env_data;
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (arg[i] && arg[i] != '=')
+	{
+		count++;
+		i++;
+	}
+	while (env_list)
+	{
+		env_data = env_list->var;
+		if (ft_strncmp(env_list->var, arg, count) == 0)
+				return (env_list);
+		env_list = env_list->next;
+	}
+	return (NULL);
 }
 
 int	unset_handler(t_shell *store)
@@ -44,7 +67,7 @@ int	unset_handler(t_shell *store)
 		printf("unset: %s: invalid parameter name\n", arg);
 	else
 	{
-		current_env = get_env_loc(store->env, arg);
+		current_env = get_target_loc(store->env, arg);
 		perform_unset(current_env, store);
 	}
 	return (EXIT_SUCCESS);
