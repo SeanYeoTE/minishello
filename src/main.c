1bed51bb3cad5f34796bf7c772f92b2a9a9e07e5
sean <tseensean@gmail.com>
2024-09-27 12:43:43 +0800

    changes to how envp is init, allows clear to be run successfully

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: seayeo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:11:01 by seayeo            #+#    #+#             */
-/*   Updated: 2024/09/24 12:42:50 by seayeo           ###   ########.fr       */
+/*   Updated: 2024/09/27 12:42:25 by seayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,44 @@ void	free_env(t_env **env)
 	*env = NULL;
 }
 
+static int	count_env(char **envp)
+{
+	int	count;
+
+	count = 0;
+	while (envp[count])
+		count++;
+	return (count);
+}
+
+static char	**init_environment(char **envp)
+{
+	char	**new_env;
+	int		i;
+	int		env_count;
+
+	env_count = count_env(envp);
+	new_env = (char **)malloc(sizeof(char *) * (env_count + 1));
+	if (!new_env)
+	{
+		perror("Failed to allocate memory for environment");
+		exit(1);
+	}
+	i = 0;
+	while (i < env_count)
+	{
+		new_env[i] = ft_strdup(envp[i]);
+		if (!new_env[i])
+		{
+			perror("Failed to copy environment variable");
+			exit(1);
+		}
+		i++;
+	}
+	new_env[env_count] = NULL;
+	return (new_env);
+}
+
 void	init_var(t_shell *store, t_env *env_head, t_var *var_head, char **envp)
 {		
 	store->input_fd = STDIN_FILENO;
@@ -40,15 +78,12 @@ void	init_var(t_shell *store, t_env *env_head, t_var *var_head, char **envp)
 	store->tail = NULL;
 	store->fd_in = STDIN_FILENO;
 	store->quotes = false;
-
 	store->cmd_head = NULL;
 	store->cmd_tail = NULL;
-	
 	store->path = getenv("PATH");
 	store->paths = ft_split(store->path, ':');
-	store->envp = ft_split(store->path, ':');
+	store->envp = init_environment(envp);
 	store->pid = NULL;
-
 	store->env = env_head;
 	store->var = var_head;
 }
@@ -63,7 +98,6 @@ int	main(int argc, char **argv, char **envp)
 		perror("run without args");
 		return (0);
 	}
-
 	env_head = env_init(&store, envp);
 	init_var(&store, env_head, NULL, envp);
 	return (prompter(&store, env_head, NULL));
