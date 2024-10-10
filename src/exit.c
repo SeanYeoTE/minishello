110980da6c31b5eb12b955f26c8609cf5fb827a7
Melvin <melvinchw96@gmail.com>
2024-10-10 20:54:10 +0800

    fully handled exit handler stderr and exit code to figure out stdout

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: melvin <melvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 15:04:19 by mchua             #+#    #+#             */
-/*   Updated: 2024/10/10 19:42:36 by melvin           ###   ########.fr       */
+/*   Updated: 2024/10/10 20:53:19 by melvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,19 @@ int	exit_handler(t_shell *store)
 	status = 0;
 	if (!one_cmd(store->cmd_head->command)) // means got more than 1 variable
 	{
-		if (!is_numeric(store->cmd_head->command->next->data)) //non numeric
+		if (ft_strcmp(store->cmd_head->command->next->data, "") == 0)
 		{
-			fprintf(stderr, "%s: %s: numeric argument required\n", store->cmd_head->command->data, store->cmd_head->command->next->data);
+
+		}
+		if (!is_numeric(store->cmd_head->command->next->data) || ft_strcmp(store->cmd_head->command->next->data, "") == 0) //non numeric
+		{
+			fprintf(stderr, "exit\n%s: %s: numeric argument required\n", store->cmd_head->command->data, store->cmd_head->command->next->data);
 			free_all(store);
 			exit (2);
 		}
 		else if(store->cmd_head->command->next->next != NULL) //more than 2 argument
 		{
-			fprintf(stderr, "%s\n%s: too many arguments\n", store->cmd_head->command->data, store->cmd_head->command->data);
+			fprintf(stderr, "exit\n%s: too many arguments\n", store->cmd_head->command->data);
 			free_all(store);
 			exit (BUILTIN_FAILURE);
 		}
